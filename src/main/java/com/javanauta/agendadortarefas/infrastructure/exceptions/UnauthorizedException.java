52562e8dd1f02179248637b82a41f6a446b89f15
Delfalas <vinidantas07@hotmail.com>
2026-02-25 20:49:51 -0300

    some settings

---
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,14 @@
+package com.javanauta.agendadortarefas.infrastructure.exceptions;
+
+import javax.naming.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable throwable) {
+        super(message);
+    }
+}
